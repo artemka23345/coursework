8c241a241081826b164d23ed23582aaceea61caf
Inv <artem@tarabakin.ru>
2023-09-24 16:22:37 +0300

    stopped at the counter

--- src/Employee.java
+++ src/Employee.java
@@ -1,18 +1,58 @@
+      /*  1. Создать класс Employee, который содержит информацию о Ф.И.О., отделе и зарплате сотрудника. Отделы для простоты должны быть названы от 1 до 5.
+        2. Добавить статическую переменную-счетчик, которая будет отвечать за id.
+        3. Добавить в класс Employee поле id, которое проставляется из счетчика, а затем счетчик увеличивает свое значение.
+        4. Добавить возможность получать значения полей из Employee (геттеры) для всех полей.
+        5. Добавить возможность устанавливать значения полей отдела и зарплаты (сеттеры).
+        6. По умолчанию все поля должны передавать через конструктор (кроме id) и заполняться в нем (включая id, который нужно получить из счетчика).
+        7. Создать внутри класса с методом main поле типа Employee[10], которое будет выполнять роль «хранилища» для записей о сотрудниках.
+        8. Создать статические методы, которые будут взаимодействовать с массивом и предоставлять результат:
+        1. Получить список всех сотрудников со всеми имеющимися по ним данными (вывести в консоль значения всех полей (toString)).
+        2. Посчитать сумму затрат на зарплаты в месяц.
+        3. Найти сотрудника с минимальной зарплатой.
+        4. Найти сотрудника с максимальной зарплатой.
+        5. Подсчитать среднее значение зарплат (можно использовать для этого метод из пункта b).
+        6. Получить Ф. И. О. всех сотрудников (вывести в консоль).
+        */
 
 public class Employee {
-    String name;
-    String patronymic;
-    String surname;
-    int salary;
-    int department;
-    static int id;
+    private String name;
+    private String patronymic;
+    private String surname;
+    private int salary;
+    private int department;
+    static int id = 0;
+
+    public static void main(String[] args) {
+        Employee employee1 = new Employee("ivan","Ivanov","Ivanovich",40000,1);
+        Employee employee2 = new Employee("ivan","Ivanov","Ivanovich",40000,1);
+        Employee employee3 = new Employee("ivan","Ivanov","Ivanovich",40000,1);
+        System.out.println(employee1);
+        System.out.println(employee2);
+        System.out.println(employee3);
+
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + getName() + '\'' +
+                ", patronymic='" + getPatronymic() + '\'' +
+                ", surname='" + getSurname() + '\'' +
+                ", salary=" + getSalary() +
+                ", department=" + getDepartment() +
+                ", id=" + this.id +
+                '}';
+    }
+
     public Employee(String name, String patronymic, String surname, int salary, int department) {
+        id++;
         this.name = name;
         this.patronymic = patronymic;
         this.surname = surname;
         this.salary = salary;
         this.department = department;
-        id =+1;
+
+
     }
     public String getName() {
         return name;
@@ -44,16 +84,4 @@ public class Employee {
     public void setDepartment(int department) {
         this.department = department;
     }
-    public static int getId() {
-        return id;
-    }
-    public static void setId(int id) {
-        Employee.id = id;
-    }
-
-    public static void main(String[] args) {
-
-
-
-        }
-    }
+}
